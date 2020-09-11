ea2d89579dccf3e49c771001321823ed1a21aa1f
Oskar Kanstedt <kaos20jl@student.ju.se>
2020-09-11 09:01:01 +0200

    backup

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ const content = document.getElementById("content")
 const välkommenTemplate = document.getElementById("välkommenTemplate")
 
 if (checklogin()){
-    applyTemplate (välkommenTemplate)
-    document.getElementById("loggaUt").addEventListener("click", logout)
+    välkommenvy()
 }
 
  else {
-    applyTemplate (startsidaTemplate)
-    document.getElementById("loggaIn").addEventListener("click", login)
+    startVy()
  }
 
 //skapat funktion för templates för att kunna använda vid anrop av vyer
@@ -25,6 +23,7 @@ function applyTemplate(template) {
 
 //skapat funktion för att storea användarnamn och lösenord
 function store(namn1, lösenord1) {
+    console.log(namn1, lösenord1)
     localStorage.setItem("inMatatNamn", namn1)
     localStorage.setItem("inMatatLösenord", lösenord1)
 }
@@ -36,27 +35,25 @@ function login(){
     const inMatatLösenord = document.getElementById("inMatatLösenord")
 
     if (inMatatNamn.value == namn && inMatatLösenord.value == lösenord) {
-        applyTemplate (välkommenTemplate)
-        store (inMatatNamn.value && inMatatLösenord.value)    
-        document.getElementById("loggaUt").addEventListener("click", logout)   
+        välkommenvy()
+        store (inMatatNamn.value, inMatatLösenord.value)    
     }
 
     else {
-        applyTemplate (felTemplate)
-        document.getElementById("försökIgen").addEventListener("click", tillbaka)
+        felVy()
     }
 }
 
 //skapat en funktion för att komma tillbaka till startsidan om man har skrivit fel användarnamn eller lösenord
 function tillbaka() {
-    applyTemplate (startsidaTemplate)
+    startVy()
 }
 
 //skapat en funktion för att ta bort värdena från localstorage
 function logout(){
-    localStorage.removeItem(inMatatNamn.value)
-    localStorage.removeItem(inMatatLösenord.value)
-    applyTemplate (startsidaTemplate)
+    localStorage.removeItem("inMatatNamn")
+    localStorage.removeItem("inMatatLösenord")
+    startVy()
 }
 
 //en funktion för att se om användaren är inloggad, alltså om användarnamn och lösenord
@@ -64,9 +61,25 @@ function logout(){
 function checklogin(){
     const inMatatNamn = localStorage.getItem("inMatatNamn")
     const inMatatLösenord = localStorage.getItem("inMatatLösenord")
-
-    return inMatatNamn.value == namn && inMatatLösenord.value == lösenord
+    console.log(inMatatNamn, inMatatLösenord)    
+    return inMatatNamn == namn && inMatatLösenord == lösenord
 }
+
+function startVy(){
+    applyTemplate(startsidaTemplate)
+    document.getElementById("loggaIn").addEventListener("click", login)
+}
+
+function välkommenvy(){
+    applyTemplate(välkommenTemplate)
+    document.getElementById("loggaUt").addEventListener("click", logout)
+}
+
+function felVy(){
+    applyTemplate(felTemplate)
+    document.getElementById("försökIgen").addEventListener("click", tillbaka)
+}
+
 
 
 
