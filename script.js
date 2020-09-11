4f8a6aa866f11a5d7d2368c964785854946b7625
Oskar Kanstedt <kaos20jl@student.ju.se>
2020-09-11 09:23:13 +0200

    backup

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//intiera const för användarnamn och lösenord, samt hämta templates och values från index
+//intiera const för användarnamn och lösenord, samt hämta templates och values från html filen
 const namn = "test";
 const lösenord = "1234";
 const felTemplate = document.getElementById("felTemplate")
@@ -6,6 +6,16 @@ const startsidaTemplate = document.getElementById("startsidaTemplate")
 const content = document.getElementById("content")
 const välkommenTemplate = document.getElementById("välkommenTemplate")
 
+//en funktion för att se om användaren är inloggad, alltså om användarnamn och lösenord
+//finns storeat i localstorage, om variablerna inte är tomma så är användaren inloggad
+function checklogin(){
+    const inMatatNamn = localStorage.getItem("inMatatNamn")
+    const inMatatLösenord = localStorage.getItem("inMatatLösenord")
+    console.log(inMatatNamn, inMatatLösenord)    
+    return inMatatNamn == namn && inMatatLösenord == lösenord
+}
+
+//mha if satsen tittar jag om checklogin är true, om så är fallet så är användaren fortfarande inloggad
 if (checklogin()){
     välkommenvy()
 }
@@ -28,8 +38,8 @@ function store(namn1, lösenord1) {
     localStorage.setItem("inMatatLösenord", lösenord1)
 }
 
-//skapat en funktion med en ifsats som loggar in användaren om användarnamn och lösenord stämmer
-//annars skickas användaren till en felsida, localstorage sparar även variabel values
+//skapat en funktion med en if sats som loggar in användaren om användarnamn och lösenord stämmer
+//annars skickas användaren till en felsida, localstorage sparar även variabel values 
 function login(){    
     const inMatatNamn = document.getElementById("inMatatNamn")
     const inMatatLösenord = document.getElementById("inMatatLösenord")
@@ -49,22 +59,16 @@ function tillbaka() {
     startVy()
 }
 
-//skapat en funktion för att ta bort värdena från localstorage
+//skapat en funktion för att ta bort värdena från localstorage som används när man loggar ut
 function logout(){
     localStorage.removeItem("inMatatNamn")
     localStorage.removeItem("inMatatLösenord")
     startVy()
 }
 
-//en funktion för att se om användaren är inloggad, alltså om användarnamn och lösenord
-//finns storeat i localstorage, om variablerna inte är tomma så är användaren inloggad
-function checklogin(){
-    const inMatatNamn = localStorage.getItem("inMatatNamn")
-    const inMatatLösenord = localStorage.getItem("inMatatLösenord")
-    console.log(inMatatNamn, inMatatLösenord)    
-    return inMatatNamn == namn && inMatatLösenord == lösenord
-}
-
+//jag skapade här funktioner till alla templates, detta eftersom att 
+//när jag loggade in så kunde jag inte logga ut, lyssnarna var inte aktiva längre alltså document.geteleme...
+//men med dessa 3 funktioner så fungerade det 
 function startVy(){
     applyTemplate(startsidaTemplate)
     document.getElementById("loggaIn").addEventListener("click", login)
